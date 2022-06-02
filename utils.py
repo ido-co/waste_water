e959672380f5159d71011b43fc1c0548a3eb53e2
ido-co <its.ido@gmail.com>
2022-06-02 19:30:13 +0300

    added augmentations to mapper

--- utils.py
+++ utils.py
@@ -4,13 +4,16 @@ from detectron2.engine import DefaultTrainer
 from detectron2.engine.hooks import HookBase
 from detectron2.evaluation import inference_context, COCOEvaluator
 from detectron2.utils.logger import log_every_n_seconds
-from detectron2.data import DatasetMapper, build_detection_test_loader
+from detectron2.data import DatasetMapper, build_detection_test_loader, build_detection_train_loader
 import detectron2.utils.comm as comm
+from detectron2.data import detection_utils as utils
+import detectron2.data.transforms as T
 import torch
 import time
 import datetime
 import logging
 import gdown
+import copy
 
 
 
@@ -94,7 +97,36 @@ class LossEvalHook(HookBase):
         self.trainer.storage.put_scalars(timetest=12)
 
 
+def custom_mapper(dataset_dict):
+    dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+    image = utils.read_image(dataset_dict["file_name"], format="BGR")
+    transform_list = [
+        T.RandomBrightness(0.7, 1),
+        T.RandomContrast(0.6, 1.3),
+        T.RandomSaturation(0.8, 1.4),
+        T.RandomRotation(angle=[20, 180], expand=True, sample_style='range'),
+        T.RandomLighting(0.7),
+        T.RandomCrop("relative", (0.5, 0.8)),
+        T.RandomFlip(prob=0.4, horizontal=False, vertical=True),
+    ]
+    image, transforms = T.apply_transform_gens(transform_list, image)
+    dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
+
+    annos = [
+        utils.transform_instance_annotations(obj, transforms, image.shape[:2])
+        for obj in dataset_dict.pop("annotations")
+        if obj.get("iscrowd", 0) == 0
+    ]
+    instances = utils.annotations_to_instances(annos, image.shape[:2])
+    dataset_dict["instances"] = utils.filter_empty_instances(instances)
+    return dataset_dict
+
+
 class MyTrainer(DefaultTrainer):
+    @classmethod
+    def build_train_loader(cls, cfg):
+        return build_detection_train_loader(cfg, mapper=custom_mapper)
+
     @classmethod
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
         cls.cfg = cfg
