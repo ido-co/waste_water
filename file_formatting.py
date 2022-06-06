e9bab393588a6380db75a42fa17ab408ad461a03
jacob <jacob.o.gidron@gmail.com>
2022-06-06 16:22:29 +0300

    add argparse to file_formatting.py

--- file_formatting.py
+++ file_formatting.py
@@ -10,28 +10,31 @@ def safe_make_dir(name, basepath=''):
         os.mkdir(dir_path)
 
 
-
-
 def flatten_to_dir(path, new_dir):
     for file in os.listdir(path):
         n_path = os.path.join(path, file)
         if os.path.isdir(n_path):
-            flatten_to_dir(n_path,new_dir)
-        elif re.search(r"\.png$",file):
-            shutil.copyfile(n_path, os.path.join(new_dir,file))
+            flatten_to_dir(n_path, new_dir)
+        elif re.search(r"\.png$", file):
+            shutil.copyfile(n_path, os.path.join(new_dir, file))
 
 
 if __name__ == '__main__':
-    low_res_path = r"C:\Users\Administrator\PycharmProjects\ml_workshop\wastwatwer_data\Waste_Water_2.v21-for_cs_workshop_v2.coco\train"
-    high_q_dir_path = r"C:\Users\Administrator\PycharmProjects\ml_workshop\wastwatwer_data\wastwatwer"
-    new_high_q_dir_path = "wastewater_all_out_ver"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("orig_path", type=str, help="the high res imeges")
+    parser.add_argument("dataset_path", type=str, help="where the coco.json is located")
+    parser.add_argument("dst_path", type=str, help="where you want the output")
+    args = parser.parse_args()
+    low_res_path = args.dataset_path
+    high_q_dir_path = args.orig_path
+    new_high_q_dir_path = args.dst_path
     # first copy the data to a new file
     safe_make_dir(new_high_q_dir_path)
-    flatten_to_dir(high_q_dir_path,new_high_q_dir_path)
+    flatten_to_dir(high_q_dir_path, new_high_q_dir_path)
     coco = json.load(open(os.path.join(low_res_path, "_annotations.coco.json")))
     high_q_dir = set(os.listdir(new_high_q_dir_path))
-    shutil.copyfile(os.path.join(low_res_path,"_annotations.coco.json"),
-                    os.path.join(new_high_q_dir_path,"_annotations.coco.json"))
+    shutil.copyfile(os.path.join(low_res_path, "_annotations.coco.json"),
+                    os.path.join(new_high_q_dir_path, "_annotations.coco.json"))
     check_dir = set(os.listdir(new_high_q_dir_path))
     os.chdir(new_high_q_dir_path)
     for img in coco['images']:
